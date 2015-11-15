29709f2b45c3ea450baf01e2734cd5548d62f149
Florian Reinhard <florian.reinhard@epfl.ch>
2015-11-15 19:16:56 +0100

    spline trajectory from list of points

---
+++ spline.py
@@ -0,0 +1,104 @@
+import sympy as sp
+import numpy as np
+import scipy as sc
+from scipy.integrate import quad
+from scipy.optimize import newton
+
+
+def _parameter_generator():
+    a, b, c, d = sp.symbols('a b c d')
+    t = sp.symbols('t')
+
+    x = a*t**3 + b*t**2 + c*t + d
+    x_dot = sp.diff(x, t)
+
+    z1, z2 = sp.symbols('z_1 z_2')
+    z1_dot, z2_dot = sp.symbols('\dot{z_1} \dot{z_2}')
+
+    lin_system = (z1 - x.subs(t, 0),
+                  z1_dot - x_dot.subs(t, 0),
+                  z2 - x.subs(t, 1),
+                  z2_dot - x_dot.subs(t, 1))
+    result = sp.solvers.solve(lin_system, a, b, c, d)
+
+    return sp.lambdify((z1, z2, z1_dot, z2_dot),
+                       [result[a], result[b], result[c], result[d]],
+                       "numpy")
+
+def _spline_generator(parameters):
+    return lambda t: (parameters[0]*t**3 + parameters[1]*t**2 +
+                      parameters[2]*t + parameters[3])
+
+def _spline_dot_generator(parameters):
+    return lambda t: (3 * parameters[0]*t**2 + 2 * parameters[1]*t +
+                      parameters[2])
+
+def _spline_dot_dot_generator(parameters):
+    return lambda t: (6 * parameters[0]*t + 2 * parameters[1])
+
+generate_spline = _parameter_generator()
+
+
+class SplineTrajectorySegment():
+    def __init__(self, start_pt, end_pt, start_dir, end_dir, roundness=0.1):
+        self.start_point = np.array(start_pt)
+        self.end_point = np.array(end_pt)
+        self.start_dir = np.array(start_dir) / np.linalg.norm(start_dir)
+        self.end_dir = np.array(end_dir) / np.linalg.norm(end_dir)
+
+        distance = np.linalg.norm(self.end_point - self.start_point)
+
+        self.start_dir *= roundness / distance
+        self.end_dir *= roundness / distance
+
+        self.parameters = [generate_spline(*conditions) for conditions in
+                           zip(start_pt, end_pt, self.start_dir, self.end_dir)]
+
+        self._spline = [_spline_generator(p) for p in self.parameters]
+        self._spline_dot = [_spline_dot_generator(p) for p in self.parameters]
+        self._spline_dot_dot = [_spline_dot_dot_generator(p)
+                               for p in self.parameters]
+        self.spline_length = self.section_length(0, 1)
+
+    def spline(self, t):
+        return [s(t) for s in self._spline]
+
+    def spline_dot(self, t):
+        return [s(t) for s in self._spline_dot]
+
+    def section_length(self, a, b):
+        ''' Evaluates the length of the spline from a to b. '''
+        return quad(lambda t: np.linalg.norm(self.spline_dot(t)), a, b)[0]
+
+    def parametrization_at_length(self, s):
+        return newton(lambda t: self.section_length(0, t) - s, 0.5)
+
+
+class SplineTrajectory():
+    def __init__(self, points, start_dir=None, end_dir=None):
+        if start_dir is None:
+            start_dir = points[1] - points[0]
+
+        if end_dir is None:
+            end_dir = points[-1] - points[-2]
+
+        directions = [self.direction_at_point(*pts) for pts
+                      in zip(points, points[1:], points[2:])]
+
+        directions = [start_dir] + directions + [end_dir]
+
+        self.splines = [SplineTrajectorySegment(start, end, start_dir, end_dir)
+                        for start, end, start_dir, end_dir
+                        in zip(points, points[1:], directions, directions[1:])]
+
+    @staticmethod
+    def direction_at_point(point_before, point, point_after):
+        v1 = point - point_before
+        v2 = point_after - point
+
+        v1_norm = np.linalg.norm(v1)
+        v2_norm = np.linalg.norm(v2)
+
+        direction = (v1 / v1_norm**2 + v2 / v2_norm**2)
+
+        return direction / np.linalg.norm(direction)
---
+++ test_spline.py
@@ -0,0 +1,72 @@
+import spline
+import unittest
+import numpy.testing as npt
+import numpy as np
+
+def normalize(v):
+    return np.array(v) / np.linalg.norm(v)
+
+class SplineTest(unittest.TestCase):
+
+    def test_start_and_end_postions_1D(self):
+        trajectory = spline.SplineTrajectorySegment([0], [1], [1], [1])
+        npt.assert_almost_equal([0], trajectory.spline(0))
+        npt.assert_almost_equal([1], trajectory.spline(1))
+
+    def test_start_and_end_postions_3D(self):
+        trajectory = spline.SplineTrajectorySegment([0, 1, 2], [-1, -2, -3], [1, 1, 1], [1, 1, 1])
+        npt.assert_almost_equal([0, 1, 2], trajectory.spline(0))
+        npt.assert_almost_equal([-1, -2, -3], trajectory.spline(1))
+
+    def test_start_and_end_derivatives_3D(self):
+        d1 = [1, 1, 1]
+        d2 = [1, 2, 3]
+        trajectory = spline.SplineTrajectorySegment([0, 1, 2], [-1, -2, -3], d1, d2)
+        npt.assert_almost_equal(normalize(d1), normalize(trajectory.spline_dot(0)))
+        npt.assert_almost_equal(normalize(d2), normalize(trajectory.spline_dot(1)))
+
+    def test_section_length(self):
+        trajectory = spline.SplineTrajectorySegment([0, 1, 2], [-1, -2, -3], [1, 1, 1], [1, 1, 1])
+        sa = trajectory.section_length(0, 0.5)
+        sb = trajectory.section_length(0.5, 1)
+        sab = trajectory.section_length(0, 1)
+        self.assertAlmostEqual(sa + sb, sab)
+
+    def test_parametrization_at_length(self):
+        trajectory = spline.SplineTrajectorySegment([0, 1, 2], [-1, -2, -3], [1, 1, 1], [1, 1, 1])
+        r = trajectory.parametrization_at_length(trajectory.spline_length / 3)
+        length = trajectory.section_length(0, r)
+        self.assertAlmostEqual(trajectory.spline_length / 3, length)
+
+
+class SplineTrajectoryTest(unittest.TestCase):
+    def test_direciton_at_point(self):
+        p1 = np.array([-1, -1])
+        p2 = np.array([0, 0])
+        p3 = np.array([1, -1])
+        expected_dir = np.array([1, 0])
+        computed_dir = spline.SplineTrajectory.direction_at_point(p1, p2, p3)
+
+        npt.assert_almost_equal(expected_dir, computed_dir)
+
+    def test_spline_segments(self):
+        p1 = np.array([0, 0])
+        p2 = np.array([21, 42])
+        p3 = np.array([88, 88])
+
+        pts = [p1, p2, p3]
+
+        p1_dir = p2 - p1
+        p2_dir = spline.SplineTrajectory.direction_at_point(p1, p2, p3)
+        p3_dir = p3 - p2
+
+        trajectory = spline.SplineTrajectory(pts)
+
+        npt.assert_almost_equal(p1, trajectory.splines[0].spline(0))
+        npt.assert_almost_equal(normalize(p1_dir), normalize(trajectory.splines[0].spline_dot(0)))
+        npt.assert_almost_equal(p2, trajectory.splines[0].spline(1))
+        npt.assert_almost_equal(p2, trajectory.splines[1].spline(0))
+        npt.assert_almost_equal(normalize(p2_dir), normalize(trajectory.splines[0].spline_dot(1)))
+        npt.assert_almost_equal(normalize(p2_dir), normalize(trajectory.splines[1].spline_dot(0)))
+        npt.assert_almost_equal(p3, trajectory.splines[1].spline(1))
+        npt.assert_almost_equal(normalize(p3_dir), normalize(trajectory.splines[1].spline_dot(1)))
