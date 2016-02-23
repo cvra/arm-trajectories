f2f7913675bbeec34dc9210b8f3a00cdce61bcc4
Florian Reinhard <florian.reinhard@epfl.ch>
2016-02-23 09:33:20 +0100

    untested generate_velocity_profile

--- trajectory.py
+++ trajectory.py
@@ -28,3 +28,84 @@ def max_acceleration_along_spline(point, velocity, acc_limits):
     direction = np.array(point.direction)
     limits_translated = acc_limits - centripetal_acceleration
     return np.linalg.norm(limit_in_direction(direction, limits_translated[0], limits_translated[1]))
+
+def generate_velocity_profile(points,
+                              sampling_distance,
+                              actuator_acc_limit,
+                              v_start=0,
+                              v_end=0,
+                              velocity_limit=0):
+    """TODO: Docstring for generate_velocity_profile.
+
+    :points: array of spline points
+    :sampling_distance: constant distance between points
+    :actuator_acc_limit: returns min/max acceleration for each actuator in function of pos & vel
+    :v_start: velocity at first point
+    :v_end: velocity at last point
+    :velocity_limit: overall maximal velocity (unlimited when zero)
+    :returns: TODO
+
+    """
+
+    def required_acceleration(v1, v2, distance):
+        ''' calculates the requierd constant acceleration between two points with given
+            velocities and distances in between. '''
+        return 0.5 * (v2 - v1) * (v1 + v2) / distance
+
+    def velocity_after_distance(v, distance, acceleration):
+        ''' calculates the velocity after accelerating constantly on a certain distance. '''
+        return np.sqrt(2.0 * distance * acceleration + v**2)
+
+    nb_samples = len(points)
+    velocity_limits = np.ones(nb_samples) * velocity_limit
+    velocity_limits[-1] = v_end
+
+    for i in reversed(range(1, nb_samples-1)):
+        if velocity_limit == 0:
+            decceleration_vel_limit_to_break_limit = 0
+        else:
+            decceleration_vel_limit_to_break_limit = required_acceleration(velocity_limits[i+1],
+                                                                           velocity_limits[i],
+                                                                           sampling_distance)
+
+        velocity = points[i+1].direction / np.linalg.norm(points[i+1].direction) * velocity_limits[i+1]
+        acc_limits = actuator_acc_limit(points[i+1].position, -velocity)
+        decceleration_limit = max_acceleration_along_spline(points[i+1].position,
+                                                            velocity_limits[i+1],
+                                                            acc_limits)
+
+        if decceleration_vel_limit_to_break_limit > decceleration_limit:
+            velocity_limits[i] = velocity_after_distance(velocity_limits[i+1],
+                                                         sampling_distance,
+                                                         decceleration_limit)
+        else:
+            pass #velocity is velocity_limit
+
+    velocity_limits[0] = v_start
+    acceleration = np.zeros(nb_samples)
+
+    for i in range(1, nb_samples):
+        if velocity_limit == 0:
+            acceleration_vel_to_break_limit = 0
+        else:
+            acceleration_vel_to_break_limit = required_acceleration(velocity_limits[i-1],
+                                                                    velocity_limits[i],
+                                                                    sampling_distance)
+
+        velocity = points[i-1].direction / np.linalg.norm(points[i-1].direction) * velocity_limits[i-1]
+        acc_limits = actuator_acc_limit(points[i-1].position, velocity)
+        acceleration_limit = max_acceleration_along_spline(points[i-1].position,
+                                                            velocity_limits[i-1],
+                                                            acc_limits)
+
+        if acceleration_vel_to_break_limit > acceleration_limit:
+            velocity_limits[i] = velocity_after_distance(velocity_limits[i-1],
+                                                         sampling_distance,
+                                                         acceleration_limit)
+            acceleration[i-1] = acceleration_limit
+        else:
+            acceleration[i-1] = acceleration_vel_to_break_limit
+
+    #TODO resample in time
+
+    return velocity_limits, acceleration
