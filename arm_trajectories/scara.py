742e56ca7bb466b7121ef55179d0fcc5c1d00eec
Florian Reinhard <florian.reinhard@epfl.ch>
2016-05-18 21:46:50 +0200

    Proper SCARA foward and inverse kinematics

--- arm_trajectories/scara.py
+++ arm_trajectories/scara.py
@@ -1,22 +1,36 @@
+''' README
+    Forward and inverse kinematics for a SCARA robotic arm.
+    `pos` is either `[x, y, z]` in cartesian space or `[alpha, beta, z]` in
+    actuator space. With `alpha` being the angle of the upper arm to the
+    `x` axis and `beta` the angle between upper and lower arm.
+    The parameters `a`, `b`, and `c` are length of upper arm, length of lower
+    arm, and a scaling factor for the `z` axis respectively.
+    `c` maps from cartesian space to the actuator space, so its unit is usually
+    `2pi / meter` or the inverse of the pitch of the screw times `2pi`.
+    The `limits` is in the form of `[[alpha_min, alpha_max], [beta_min, ...], ...]`.
+    Solution with a positive `beta` will always be the fist choice when it
+    is valid.
+'''
 import numpy as np
 
-def forward_kinematics(pos, a, b):
+def forward_kinematics(pos, a, b, c):   # c is in 2pi*rad/m
     alpha = pos[0]
     beta = pos[1]
-    #z = pos[2]
+    z = pos[2]
 
     x = a * np.cos(alpha) + b * np.cos(alpha + beta)
     y = a * np.sin(alpha) + b * np.sin(alpha + beta)
 
-    return (x, y)
+    return (x, y, c*z)
 
 def inside_limits(v, limits):
     return np.all([dim >= lim[0] and dim <= lim[1] for (dim, lim) in zip(v, limits)])
 
-def inverse_kinematics(pos, a, b, limits):
+# limits is an array of [min, max]
+def inverse_kinematics(pos, a, b, c, limits):
     x = pos[0]
     y = pos[1]
-    #z = pos[2]
+    z = pos[2]/c
 
     dist_dist = x**2 + y**2
     dist = np.sqrt(dist_dist)
@@ -32,10 +46,10 @@ def inverse_kinematics(pos, a, b, limits):
     alpha = np.arctan2(y, x) - np.arccos((dist_dist + a**2 - b**2) / (2*a*dist))
     beta = np.arccos((dist_dist - a**2 - b**2) / (2 * a * b))
 
-    if not inside_limits((alpha, beta), limits):
+    if not inside_limits((alpha, beta, z), limits):
         alpha = 2 * np.arctan2(y, x) - alpha
         beta *= -1
-        if not inside_limits((alpha, beta), limits):
+        if not inside_limits((alpha, beta, z), limits):
             raise ValueError("Solution for {} violates limits".format(pos))
 
-    return (alpha, beta)
+    return (alpha, beta, z)
